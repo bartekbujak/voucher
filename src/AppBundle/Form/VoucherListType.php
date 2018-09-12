aada448918471d4d6863e16bb6c18267109b6481
Bartłomiej Bujak <bbujak@mennica.com.pl>
2018-09-12 10:56:27 +0200

    integertype

--- src/AppBundle/Form/VoucherListType.php
+++ src/AppBundle/Form/VoucherListType.php
@@ -5,6 +5,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 
 class VoucherListType extends AbstractType
@@ -12,8 +13,8 @@ class VoucherListType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('codeLength', TextType::class)
-            ->add('numCodesToGenerate', TextType::class)
+            ->add('codeLength', IntegerType::class)
+            ->add('numCodesToGenerate', IntegerType::class)
             ->add('fileName', TextType::class)
             ->add('save', SubmitType::class, [
                 'label' => 'Generate'
